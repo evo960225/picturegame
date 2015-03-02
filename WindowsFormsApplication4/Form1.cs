8b2fd3eceea0951a9b6d52de9b1ce26379da6aed
youyeric <youyeric@gmail.com>
2015-03-03 01:28:22 +0800

    pictureGame

---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        PicMatrix pictureshow = new PicMatrix(100, 100); 
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(1024,768);
+            pictureshow.picShow(this);
+            pictureshow.randomShow();
+            button1.Location = new Point(300,500);
+            button1.Text = "PLAY";
+            pictureshow.pic[1, 0].Click += new EventHandler(pic_Click1);
+            pictureshow.pic[1, 1].Click += new EventHandler(pic_Click2);
+            pictureshow.pic[1, 2].Click += new EventHandler(pic_Click3);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureshow.pic[1,0].BackColor=Color.Gray;
+            pictureshow.pic[1, 1].BackColor = Color.Gray;
+            pictureshow.pic[1, 2].BackColor = Color.Gray;
+            
+        }
+        private void pic_Click1(object sender, EventArgs e)
+        {
+            pictureshow.compareColor(pictureshow.pic[0,pictureshow.countright],0);
+        }
+        private void pic_Click2(object sender, EventArgs e)
+        {
+            pictureshow.compareColor(pictureshow.pic[0, pictureshow.countright], 1);
+        }
+        private void pic_Click3(object sender, EventArgs e)
+        {
+            pictureshow.compareColor(pictureshow.pic[0, pictureshow.countright], 2);
+        }
+
+    }
+}
---
+++ WindowsFormsApplication4/PicMatrix.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace WindowsFormsApplication4
+{
+    class PicMatrix
+    {
+        public PictureBox[,] pic = new PictureBox[2,3];
+        Color []c = new Color[4];
+        private int []rs=new int[3];
+        Random r = new Random();
+        private int height;
+        private int width;
+        public  int countright=0;
+        public PicMatrix(int height,int width)
+        {
+            c[0] = Color.Blue;
+            c[1] = Color.Red;
+            c[2] = Color.Green;
+            c[3] = Color.Yellow;
+            rs[0] = r.Next(0, 3);
+            rs[1] = r.Next(0, 3);
+            rs[2] = r.Next(0, 3);
+
+            this.height = height;
+            this.width = width;
+        }
+        public void picShow(Form form1)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    pic[j, i] = new PictureBox();
+                    pic[j, i].Size = new Size(height, width);
+                    pic[j, i].Location = new Point(300 + i * height, 200 + j * width);
+                    pic[j, i].BorderStyle = BorderStyle.Fixed3D;
+                    form1.Controls.Add(pic[j, i]);
+                    pic[j, i].Visible = false;
+                }
+
+                
+            }
+        }
+        public void randomShow()
+        {
+            for(int j =0;j<2;j++)
+            {
+                for(int i=0;i<3;i++)
+                {
+                    pic[j, i].BackColor = c[rs[i]];
+                    pic[j, i].Visible = true;
+                }
+                this.randomColor();
+            }
+        }
+        public void randomColor()
+        {
+            int rr,temp;
+            for(int i=0;i<rs.Length;i++)
+            {
+                rr = r.Next(0,3);
+                temp = rs[i];
+                rs[i] = rs[rr];
+                rs[rr] = temp;
+            }
+        }
+        public void resetColor()
+        {
+            for(int i=0;i<rs.Length;i++)
+            {
+                rs[i] = r.Next(0, 3);
+            }
+        }
+        public void compareColor(PictureBox p,int i)
+        {
+            if(countright==3)
+            {
+                countright = 0;
+                this.resetColor();
+                this.randomShow();
+            }
+            else if(p.BackColor.Equals(c[rs[i]]))
+            {
+                pic[1, i].BackColor = c[rs[i]];
+                countright=countright+1;
+            }
+            else
+            {
+                countright = 0;
+                this.resetColor();
+                this.randomShow();
+            }
+        }
+        
+    }
+}
